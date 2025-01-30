eb65f065df2e4e60e65d16c7b754b9e3d8232221
derlole <122916573+derlole@users.noreply.github.com>
2025-01-30 12:43:12 +0000

    app

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.listen(PORT, () => {
 
 /*
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Example app listening at http://0.0.0.0:${PORT}`);
+  console.log(`webGartyParty running on  http://0.0.0.0:${PORT}`);
 }); 
 */
